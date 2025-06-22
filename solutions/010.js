e4e9899f06722e3a89730929ddb9b8d3d64e885e
Andrei(@elektrik) <101565487+bartcevich@users.noreply.github.com>
2025-06-22 16:43:02 +0300

    Update 010.js

--- solutions/010.js
+++ solutions/010.js
@@ -1,23 +1,29 @@
-function postTweet(tweetText){
-    return new Promise((resolve, reject) => {
-  setTimeout(()=> {if(Math.random()<0.7) resolve(`Твит '${tweetText}' успешно опубликован!`);
-      else reject(new Error("Ошибка сети при публикации"));
-  },1000)})
+async function postTweet(tweetText) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (Math.random() < 0.7) {
+        resolve(`Твит '${tweetText}' успешно опубликован!`);
+      } else {
+        reject(new Error("Ошибка сети при публикации"));
+      }
+    }, 1000);
+  });
 }
 
-async function publish(){
-    try{
-        const res = await postTweet("Мой первый твит с async/await!");
-        console.log(res)
-    }
-    catch(error)
-    {
-        console.log(error.message)
-    }
-    finally
-    {
-        console.log("Операция публикации завершена (успех или ошибка)")
+async function publish() {
+    let postStatus = 'unknown_error';
+  try {
+    const result = await postTweet("Мой первый твит с async/await!");
+    postStatus = 'Успех';
+    console.log(result);
+  } catch (error) {
+    postStatus = 'Ошибка';
+    console.error("Ошибка:", error.message);
+  } finally {
 
-    }
+    console.log(`Операция публикации завершена: '${postStatus}'`);
+  }
 }
-publish()
+
+publish();
+
